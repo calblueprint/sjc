9a5d60be61345ede5dd6e7abc85faa6d7dbe813d
Amy Shan <amyshan@berkeley.edu>
2017-10-21 13:15:37 -0700

    merged with old changes

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,67 @@
+class API::CommentsController < ApplicationController
+  respond_to :json
+
+  def show
+    if params[:id]
+      @comment = Comment.find(params[:id])
+      render json: @comment
+    end
+  end
+
+  def client_comments
+    if params[:client_id]
+      @comment = Comment.find(params[:client_id])
+      render json: @comment, each_serializer: CommentSerializer, root: false
+    end
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    begin
+      saved = comment.save!
+    rescue ActiveRecord::StatementInvalid => invalid
+      return render_json_message(:forbidden, errors: "Invalid comment")
+    end
+    if saved
+      render_json_message(:ok, message: 'Comment successfully created!')
+    else
+      render_json_message(:forbidden, errors: comment.errors.full_messages)
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.destroy
+      render_json_message(:ok, message: 'Comment successfully deleted!')
+    else
+      render_json_message(:forbidden, errors: comment.errors.full_messages)
+    end
+    # head 204
+  end
+
+  def update
+    begin
+      comment = Comment.find(params[:id])
+      a = comment.update(comment_params)
+    rescue
+      render_json_message(:forbidden)
+      return
+    end
+    if a
+      render_json_message(:ok, message: 'Comment successfully updated!')
+    else
+      render_json_message(:forbidden, errors: comment.errors.full_messages)
+    end
+  end
+
+  def comment_params
+    params.permit(
+      :id,
+      :user_id,
+      :content,
+      :thread_id,
+      :client_id,
+    )
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resources :clients, :only => [:show, :create, :update, :destroy]
+    resources :comments, :only => [:show, :create, :update, :destroy]
   end
 
   get '/', to:  'users#view_tasks'
