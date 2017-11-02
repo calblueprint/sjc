4b11fce9fc606cb1b20545a5e1d65ba6f6ba416e
Neel Somani <neel@berkeley.edu>
2017-11-01 18:07:14 -0700

    Add 422 error

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -12,15 +12,11 @@ class API::TasksController < ApplicationController
     task = Task.new(
         {:client_id => params['client_id'], :description => params['description']}
     )
-    begin
-      saved = task.save!
-    rescue ActiveRecord::StatementInvalid => invalid
-      return render(:json => {:message => "Invalid task."}.to_json)
-    end
+    saved = task.save!
     if saved
       render(:json => {:message => 'Task successfully created!'}.to_json)
     else
-      render(json: task.errors.full_messages)
+      render(json: task.errors.full_messages, :status => 422)
     end
   end
 
@@ -29,37 +25,27 @@ class API::TasksController < ApplicationController
     if task.destroy
       render(:json => {:message => 'Task successfully deleted!'}.to_json)
     else
-      render(json: client.errors.full_messages)
+      render(json: client.errors.full_messages, :status => 422)
     end
   end
 
   def assign
-    begin
-        user = User.find(params[:user_id])
-        a = user.tasks << Task.find(params[:task_id])
-    rescue
-      render(:json => {:message => "Invalid assignment."}.to_json)
-      return
-    end
+    user = User.find(params[:user_id])
+    a = user.tasks << Task.find(params[:task_id])
     if a
       render(:json => {:message => 'Task successfully updated!'}.to_json)
     else
-      render(json: user.errors.full_messages)
+      render(json: user.errors.full_messages, :status => 422)
     end
   end
 
   def unassign
-    begin
-        user = User.find(params[:user_id])
-        a = user.tasks.delete(Task.find(params[:task_id]))
-    rescue
-      render(:json => {:message => "Invalid unassign."}.to_json)
-      return
-    end
+    user = User.find(params[:user_id])
+    a = user.tasks.delete(Task.find(params[:task_id]))
     if a
       render(:json => {:message => 'Task successfully updated!'}.to_json)
     else
-      render(json: user.errors.full_messages)
+      render(json: user.errors.full_messages, :status => 422)
     end
   end
 
