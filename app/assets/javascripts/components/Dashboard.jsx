1518aaf8c15caed34ad960b9e372fae6961eb341
Michael Do <michaeldo2@gmail.com>
2017-11-06 17:29:45 -0800

    fix tabs hopefully

--- app/assets/javascripts/components/Dashboard.jsx
+++ app/assets/javascripts/components/Dashboard.jsx
@@ -3,34 +3,34 @@ class Dashboard extends React.Component {
   	const { Tab, Row, Col, Nav, NavItem } = ReactBootstrap;
   	const { user } = this.props;
   	const fullName = `${user.first_name} ${user.last_name}`
-    return (
-    	<div>
-	    	<DashboardHeader name={fullName}/>
-	    	<Tab.Container id="dashboard-nav" defaultActiveKey={1}>
-			    <Row className="clearfix">
-			      <Col sm={2}>
-			        <Nav stacked bsClass="nav dashboard-nav-tab" bsStyle="pills">
-			          <NavItem eventKey={1}>
-			            Tasks
-			          </NavItem>
-			          <NavItem eventKey={2}>
-			            Clients
-			          </NavItem>
-			        </Nav>
-			      </Col>
-			      <Col sm={6}>
-			        <Tab.Content animation>
-			          <Tab.Pane eventKey={1}>
-			            <TaskList userId={user.id} />
-			          </Tab.Pane>
-			          <Tab.Pane eventKey={2}>
-			            <ClientList />
-			          </Tab.Pane>
-			        </Tab.Content>
-			      </Col>
-			    </Row>
-			  </Tab.Container>
-	    </div>
-  	);
+		return (
+		<div>
+			<DashboardHeader name={fullName} />
+			<Tab.Container id="dashboard-nav" defaultActiveKey={1}>
+				<Row className="clearfix">
+					<Col sm={2}>
+						<Nav stacked bsClass="nav dashboard-nav-tab" bsStyle="pills">
+							<NavItem eventKey={1}>
+								Tasks
+							</NavItem>
+							<NavItem eventKey={2}>
+								Clients
+							</NavItem>
+						</Nav>
+					</Col>
+					<Col sm={6}>
+						<Tab.Content animation>
+							<Tab.Pane eventKey={1}>
+								<TaskList userId={user.id} />
+							</Tab.Pane>
+							<Tab.Pane eventKey={2}>
+								<ClientList />
+							</Tab.Pane>
+						</Tab.Content>
+					</Col>
+				</Row>
+			</Tab.Container>
+		</div>
+		);
   }
 }
