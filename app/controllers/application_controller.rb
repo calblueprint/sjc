4da08d8d56f9a8f34f72a79d3a3e5b1dcfba10b2
Calvin Dong <calvind777@gmail.com>
2017-11-06 17:21:37 -0800

    Revert "Home page design and functional log in"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  respond_to :html, :json
-  
-  def render_json_message(status, options = {})
-	  render json: {
-	    data: options[:data],
-	    message: options[:message],
-	    to: options[:to],
-	    errors: options[:errors]
-	  }, status: status
-	end
 end
