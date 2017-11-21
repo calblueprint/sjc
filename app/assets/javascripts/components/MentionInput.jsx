2b702b24b0faca0cdec69ec32b98b90a3090c78b
Calvin Dong <calvind777@gmail.com>
2017-11-20 17:34:07 -0800

    mention input fix

--- app/assets/javascripts/components/MentionInput.jsx
+++ app/assets/javascripts/components/MentionInput.jsx
@@ -2,51 +2,91 @@
 class MentionInput extends React.Component {
 
     constructor(props) {
-        super();
-        this.state = {
-            users: [],
-            usersMap: {},
-            showUsers: false,
-            mentionedUsers: [],
-            value: ''
-        }
+      super();
+      this.state = {
+        users: [],
+        originalUsers: [],
+        idToUsers: {},
+        showUsers: false,
+        mentionedUsers: [],
+        value: ''
+      }
     }
 
     componentDidMount() {
-        Requester.get('/api/users/show').then((data) => {
-            this.setState({users: data});
-            let userIdToName = {};
-            data.forEach((user) => {
-                userIdToName[user.id] = user.first_name + " " + user.last_name;
-            });
-            this.setState({usersMap:userIdToName});
+      Requester.get('/api/users/show').then((data) => {
+        this.setState({ users: data });
+        this.setState({ originalUsers: Array.from(data) })
+        let userIdToName = {};
+        data.forEach((user) => {
+            userIdToName[user.id] = user.first_name + " " + user.last_name;
         });
+        this.setState({ idToUsers: userIdToName });
+      });
     }
     
-    userClicked(evt) {
-        this.setState({
-            mentionedUsers: this.state.mentionedUsers + [evt.target.id],
-            showUsers: false,
-            value: this.state.value + this.state.usersMap[evt.target.id]
-        });
+    userClicked = (evt) => {
+      const cursor = this.textInput.selectionStart;
+      const curr = this.textInput.value;
+      const closestAt = curr.lastIndexOf('@', cursor);
+      const currentTypedUser = curr.substring(closestAt + 1, cursor + 1);
+      const clickedUser= this.state.idToUsers[evt.target.id];
+      let value = '';
+      if (!!currentTypedUser) {
+        value = curr.substring(0, closestAt + 1) + clickedUser;
+      }
+      else {
+        value = this.state.value + clickedUser;
+      }
+      const newMentioned = this.state.mentionedUsers.concat(evt.target.id);
+      this.setState({
+        mentionedUsers: newMentioned,
+        showUsers: false,
+        value: value,
+        users: Array.from(this.state.originalUsers)
+      });
     }
 
-    handleKeyPress(event) {
+    handleKeyPress = (event) => {
+      cursor = this.textInput.selectionStart;
         if (event.charCode == 64) {
-            this.setState({
-                showUsers: true
-            });
-        } else {
-            this.setState({
-                showUsers: false
-            });
+          this.setState({
+            showUsers: true,
+            users: Array.from(this.state.originalUsers)
+          });
+        } 
+        if (this.state.showUsers) {
+          const curr = this.textInput.value + event.key;
+        //  console.log(curr)
+          const closestAt = curr.lastIndexOf('@', cursor);
+         // console.log(closestAt)
+          if (closestAt != 1) {
+            const currentTypedUser = curr.substring(closestAt + 1, cursor + 1);
+            console.log("t user"+ currentTypedUser)
+            if (!!currentTypedUser) {    
+              this.setState({
+                users: this.state.originalUsers.filter( (user) => (user.first_name + " " + user.last_name).toLowerCase().indexOf(currentTypedUser.toLowerCase()) >= 0)
+              });
+            }
+          }
         }
     }
 
-    handleChange(event) {
-        this.setState({
-            value: event.target.value
-        })
+    handleChange = (event) => {
+      this.setState({
+        value: event.target.value
+      });
+    }
+
+    handleSubmit = () => {
+      let cleanedMentionedUsers = [];
+      for (let id of this.state.mentionedUsers) {
+        userName = this.state.idToUsers[id];
+        if (this.textInput.value.indexOf(userName) >= 0) {
+          cleanedMentionedUsers.push(id);
+        }
+      }
+      //send notifications to the user Ids in cleanedMentionedUsers
     }
 
     render() {
@@ -55,30 +95,31 @@ class MentionInput extends React.Component {
         let usersList = "";
 
         if (this.state.showUsers) {
-            usersList = this.state.users.map(
-                (user) => {
-                    return (
-                        <li key={user.id} id={user.id} onClick={this.userClicked.bind(this)}>
-                            {user.first_name + " " + user.last_name}
-                        </li>
-                    );
-                }
-            );
+          usersList = this.state.users.map(
+            (user) => {
+              return (
+                <li key={user.id} id={user.id} onClick={this.userClicked}>
+                  {user.first_name + " " + user.last_name}
+                </li>
+              );
+            }
+          );
         }
 
         return (
-            <div>
-                <FormGroup>
-                    <FormControl 
-                        onChange={this.handleChange.bind(this)} 
-                        onKeyPress={this.handleKeyPress.bind(this)}
-                        type="text"
-                        value={this.state.value} />
-                    <ul>
-                        {usersList}
-                    </ul>
-                </FormGroup>
-            </div>
+          <div>
+            <input
+              ref={(input) => { this.textInput = input; }}
+              onChange={this.handleChange} 
+              onKeyPress={this.handleKeyPress}
+              type="text"
+              value={this.state.value} 
+              />
+
+            <ul>
+              {usersList}
+            </ul>
+          </div>
         );
     }
 
