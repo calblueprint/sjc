3f168f82c607c45214a7d649ab894cd53cf2e4a4
Michael Do <michaeldo2@gmail.com>
2017-11-30 17:43:22 -0800

    FIX TABS

--- app/assets/javascripts/components/Header.jsx
+++ app/assets/javascripts/components/Header.jsx
@@ -1,17 +1,17 @@
 class Header extends React.Component {
-	render() {
-		const { user, imagePath } = this.props;
-		const { Image } = ReactBootstrap;
-		return (
-			<div className="sjc-header">
-				<a href="/">
-					<img className="header-logo" src={imagePath} />
-				</a>
-				<div className="header-profile">
-					{user.first_name} {user.last_name}
-					<Image src="https://cdn2.iconfinder.com/data/icons/user-interface-essentials/64/Artboard_12-512.png" circle bsClass="header-profile-img img" />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { user, imagePath } = this.props;
+    const { Image } = ReactBootstrap;
+    return (
+      <div className="sjc-header">
+        <a href="/">
+          <img className="header-logo" src={imagePath} />
+        </a>
+        <div className="header-profile">
+          {user.first_name} {user.last_name}
+          <Image src="https://cdn2.iconfinder.com/data/icons/user-interface-essentials/64/Artboard_12-512.png" circle bsClass="header-profile-img img" />
+        </div>
+      </div>
+    );
+  }
 }
