465998ea93b3eaf3232f148ccf52929fca99f2de
Eric Liang <liang.eric24@gmail.com>
2018-02-08 00:03:03 -0800

    model default state hide

--- app/assets/javascripts/templates/modal.jsx
+++ app/assets/javascripts/templates/modal.jsx
@@ -4,7 +4,9 @@ class DefaultModal extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      showModal: false,
+    };
   }
 
   openModal = () => {
