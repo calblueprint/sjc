daab0b00400187dddf84710b39d6cd20363978e9
Eric Liang <liang.eric24@gmail.com>
2018-02-11 18:22:38 -0800

    styled signup page and added more error checks

--- app/assets/javascripts/components/Register.jsx
+++ app/assets/javascripts/components/Register.jsx
@@ -3,7 +3,7 @@ class Register extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      success: '',
+      error: '',
       email: '',
       password: '',
       first_name: '',
@@ -30,6 +30,17 @@ class Register extends React.Component {
     reader.readAsDataURL(files[0]);
   }
 
+  submit = (evt) => {
+    evt.preventDefault();
+    if (!this.state.email || !this.state.password) {
+      this.setState({
+        error: 'Username or password can\'t be blank.',
+      });
+    } else {
+      this.createUser(evt);
+    }
+  }
+
   createUser = (evt) => {
     evt.preventDefault();
     const user = {
@@ -42,10 +53,11 @@ class Register extends React.Component {
     const params = {
       user: user,
     }
+
     Requester.post('/api/users', params).then((data) => {
-      this.setState({ success: 'Attorney created!' });
+      window.location.href = '/';
     }).catch((data) => {
-      this.setState({ success: 'Failed to create attorney.' });
+      this.setState({ error: 'Failed to create attorney.' });
     });
 
     return false;
@@ -53,27 +65,55 @@ class Register extends React.Component {
 
   render() {
 
-    const { Checkbox, Radio, FormGroup, ControlLabel, FormControl, Button } = ReactBootstrap;
+    const { FormGroup, ControlLabel, FormControl, Button } = ReactBootstrap;
 
+    let errorBox;
+
+    if (this.state.error) {
+      errorBox = <p className="error-msg-container">{this.state.error}</p>
+    }
 
     return (
       <div>
-        <h1>Register Attorney</h1>
-        {this.state.success}
-        <form onSubmit={this.createUser}>
-          <label>
-          Email:
-          <input name="email" type="text" value={this.state.email} onChange={this.handleChange}/>
-          </label>
-          Password:
-          <input name="password" value={this.state.password} onChange={this.handleChange}/>
-          First Name:
-          <input name="first_name" value={this.state.first_name} onChange={this.handleChange}/>
-          Last Name:
-          <input name="last_name" value={this.state.last_name} onChange={this.handleChange}/>
-          Upload a profile picture:
-          <input name="avatar" type="file" onChange={this.setFile}/>
-          <Button type="submit">
+        <form>
+          <div className="input-container marginBot-xs">
+            <label htmlFor="email" className="label label--newline">Email</label>
+            <input name="email" id="email" className="input"
+              type="text" value={this.state.email} placeholder="email@example.com"
+              onChange={this.handleChange}/>
+          </div>
+
+          <div className="input-container marginBot-lg">
+            <label htmlFor="password" className="label label--newline">Password</label>
+            <input name="password" id="password" className="input"
+              type="password" value={this.state.password}
+              onChange={this.handleChange}/>
+          </div>
+
+          <div className="input-row marginBot-xs">
+            <div className="input-container">
+              <label htmlFor="fname" className="label label--newline">First Name</label>
+              <input name="first_name" id="fname" className="input"
+                type="text" value={this.state.first_name} placeholder="Mary"
+                onChange={this.handleChange}/>
+            </div>
+
+            <div className="input-container">
+              <label htmlFor="lname" className="label label--newline">Last Name</label>
+              <input name="last_name" id="lname" className="input"
+                type="text" value={this.state.last_name} placeholder="Chen"
+                onChange={this.handleChange}/>
+            </div>
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="avatar" className="label label--newline">Upload a profile picture</label>
+            <input name="avatar" id="avatar" type="file" onChange={this.setFile}/>
+          </div>
+
+          {errorBox}
+
+          <Button type="submit" className="button marginTop-sm" onClick={this.submit}>
             Submit
           </Button>
         </form>
