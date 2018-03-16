bad6595dc6247480cb29160746c0554ce7312a59
Amy Shan <amyshan@berkeley.edu>
2018-03-16 14:13:16 -0700

    restored read notification controller function

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -58,6 +58,16 @@ class API::UsersController < ApplicationController
   	render json: notifications
   end
 
+  def read_notification
+    updated = Notification.find(params[:notif_id]).update({read: true})
+
+    if updated
+      render json: {message: 'success'}
+    else
+      render json: {message: 'fail'}
+    end
+  end
+
   def read_all_notifications
     Notification.where(id: params[:notification_ids]).update_all({read: true})
     render json: {message: 'Notifications read'}
