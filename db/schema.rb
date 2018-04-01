0360abc3b299a3e8a73fe4debd9470cae53a9c23
Franco <christopher.franco.m@gmail.com>
2018-03-31 23:58:03 -0700

    roles have replaced admin column in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331073848) do
+ActiveRecord::Schema.define(version: 20180401065146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,7 +127,6 @@ ActiveRecord::Schema.define(version: 20180331073848) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "admin", default: false
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
