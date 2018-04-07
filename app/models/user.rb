8f47cac2b21559919fc027c8bfa156d35cf01f0f
Peter Veerman <peterkangveerman@gmail.com>
2018-04-07 14:59:24 -0700

    Add missing.png

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,12 @@
 
 class User < ApplicationRecord
   has_and_belongs_to_many :tasks
-  has_attached_file :avatar, styles: { medium: "300x300>" }, default_url: "/images/missing.jpg"
+  has_attached_file :avatar, styles: { medium: "300x300>" }, default_url: "images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   has_many :notifications
 	has_many :comments
   has_many :cases
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
