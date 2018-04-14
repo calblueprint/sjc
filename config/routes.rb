5f8077e3fac1eac16fb5faedaed21e6232dee797
Franco <christopher.franco.m@gmail.com>
2018-04-14 00:17:43 -0700

    changed user profile path

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,9 @@ Rails.application.routes.draw do
   root :to => 'pages#landing'
 
   get '/users/register', to: 'users#register'
-  get '/users/edit_profile', to: 'users#edit_profile'
+
+  get 'user/:id', to: 'users#edit_profile', as: :userid
+
 
   get '/clients/new'
   get '/clients/', to: 'clients#all_clients'
