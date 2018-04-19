872d538fc0c075fd566bb9c83049838c8e7e999d
Peter Veerman <peterkangveerman@gmail.com>
2018-04-18 19:22:01 -0700

    Fix not being able to register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:register]
 
   def dashboard
     @user = current_user
